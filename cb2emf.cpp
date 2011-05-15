b83aee3ccf6bf2f57d9ff7bd5028b82a710bbdaa
Egtra <yusuke.ichinohe@gmail.com>
2011-05-16 00:14:25 +0900

    Modify error message.

--- cb2emf.cpp
+++ cb2emf.cpp
@@ -4,16 +4,21 @@ int _tmain(int argc, _TCHAR* argv[])
 {
 	if (argc <= 1)
 	{
-		std::cerr << "ファイル名を指定してください。" << std::endl;
+		std::cerr << "Usage: cb2emf output-filename.emf" << std::endl;
 		return 1;
 	}
 
 	OpenClipboard(0);
 
-	auto hemfSrc = static_cast<HENHMETAFILE>(GetClipboardData(CF_ENHMETAFILE));
-	auto hemf = CopyEnhMetaFile(hemfSrc, argv[1]);
-	DeleteEnhMetaFile(hemf);
-
+	if (auto hemfSrc = static_cast<HENHMETAFILE>(GetClipboardData(CF_ENHMETAFILE)))
+	{
+		auto hemf = CopyEnhMetaFile(hemfSrc, argv[1]);
+		DeleteEnhMetaFile(hemf);
+	}
+	else
+	{
+		std::cerr << "Cannot get EMF from clipboard." << std::endl;
+	}
 	CloseClipboard();
 	return 0;
 }
